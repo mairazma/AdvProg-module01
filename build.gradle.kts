30cc1daa6e4e87a701aa6b6c5eea5d6f47fafc7d
Maira <mairaazsh@gmail.com>
2026-02-10 06:19:52 +0700

    Unit Testing Preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,12 +36,11 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
-    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-enginr:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher") testImplementation ("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
+    testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-enginr:${junitJupiterVersion}")
 }
 
 tasks.register<Test>("unitTest") {
