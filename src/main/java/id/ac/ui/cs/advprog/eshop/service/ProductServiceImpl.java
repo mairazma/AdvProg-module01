408417583de19289aa383efdc2cbe50e7fe1310d
Maira <mairaazsh@gmail.com>
2026-02-10 06:04:56 +0700

    Feature Adding: Delete Product - Services

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -39,4 +39,9 @@ public class ProductServiceImpl implements ProductService {
     public Product findById(String productId) {
         return productRepository.findById(productId);
     }
+
+    @Override
+    public void delete(String productId) {
+        productRepository.delete(productId);
+    }
 }
