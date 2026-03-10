265c337ceecc2e332adac7130052192b506303ff
Maira <mairaazsh@gmail.com>
2026-03-10 14:24:07 +0700

    [GREEN] Implement PaymentServiceImpl

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -12,15 +12,17 @@ class PaymentTest {
     @BeforeEach
     void setUp() {
         paymentData = new HashMap<>();
-        paymentData.put("Bank Central Asia", "BCA");
-        paymentData.put("Bank Nasional Indonesia", "BNI");
+        paymentData.put("bankName", "BCA");
+        paymentData.put("referenceCode", "REF001");
     }
 
     @Test
     void testCreatePaymentSuccess() {
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", paymentData);
 
         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", payment.getId());
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getOrderId());
         assertEquals("BANK_TRANSFER", payment.getMethod());
         assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
         assertEquals(paymentData, payment.getPaymentData());
@@ -28,36 +30,49 @@ class PaymentTest {
 
     @Test
     void testCreatePaymentWithStatus() {
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", "SUCCESS", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "SUCCESS", paymentData);
 
         assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
+    void testCreatePaymentInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                    "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "MEOW", paymentData);
+        });
+    }
+
+    @Test
     void testCreatePaymentEmptyBankName() {
         paymentData.put("", "Mandiri");
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "SUCCESS", paymentData);
         assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentNullBankName() {
         paymentData.put(null, "Mandiri");
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "SUCCESS", paymentData);
         assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentEmptyReferenceCode() {
         paymentData.put("Mandiri", "");
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "SUCCESS", paymentData);
         assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentNullReferenceCode() {
         paymentData.put("Mandiri", null);
-        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6",
+                "13652556-012a-4c07-b546-54eb1396d79b", "BANK_TRANSFER", "SUCCESS", paymentData);
         assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 }
