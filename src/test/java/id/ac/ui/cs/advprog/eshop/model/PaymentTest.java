b01c5eb72accb301ac9634d7b35a8baf041b45e7
Maira <mairaazsh@gmail.com>
2026-03-10 12:57:06 +0700

    [RED] Add test for Payment model: Initialize Payment

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -14,4 +14,23 @@ class PaymentTest {
         paymentData.put("Bank Central Asia", "BCA");
         paymentData.put("Bank Nasional Indonesia", "BNI");
     }
+
+    @Test
+    void testCreatePaymentSuccess() {
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
+
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", payment.getId());
+        assertEquals("BANK_TRANSFER", payment.getMethod());
+        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentWithStatus() {
+        Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", "SUCCESS", paymentData);
+
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+
 }
