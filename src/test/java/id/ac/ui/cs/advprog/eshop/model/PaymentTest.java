eac63fbec46be30aeac090a5c10e586b0cdc5f0f
Maira <mairaazsh@gmail.com>
2026-03-10 13:25:02 +0700

    [REFACTOR] Apply PaymentStatus enum to Payment model test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
@@ -21,7 +22,7 @@ class PaymentTest {
 
         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", payment.getId());
         assertEquals("BANK_TRANSFER", payment.getMethod());
-        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
         assertEquals(paymentData, payment.getPaymentData());
     }
 
@@ -29,34 +30,34 @@ class PaymentTest {
     void testCreatePaymentWithStatus() {
         Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", "SUCCESS", paymentData);
 
-        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentEmptyBankName() {
         paymentData.put("", "Mandiri");
         Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
-        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentNullBankName() {
         paymentData.put(null, "Mandiri");
         Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
-        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentEmptyReferenceCode() {
         paymentData.put("Mandiri", "");
         Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
-        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentNullReferenceCode() {
         paymentData.put("Mandiri", null);
         Payment payment = new Payment("eb558e9f-1c39-460e-8860-71af6af63bd6", "BANK_TRANSFER", paymentData);
-        assertEquals("WAITING_PAYMENT", payment.getStatus());
+        assertEquals(PaymentStatus.WAITING_PAYMENT.getValue(), payment.getStatus());
     }
 }
